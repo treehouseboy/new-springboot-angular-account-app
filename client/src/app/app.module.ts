a1aef94a9945bf2b034687414b607e0ad312e801
George Hardy <george.hardy@hotmail.co.uk#>
2018-06-24 19:50:11 +0100

    Formatted data in simple table

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AccountService } from './shared/account/account.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AccountListComponent } from './account-list/account-list.component';
+import { MatButtonModule, MatCardModule, MatTableModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,14 @@ import { AccountListComponent } from './account-list/account-list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatTableModule,
+    MatToolbarModule
   ],
   providers: [AccountService],
   bootstrap: [AppComponent]
